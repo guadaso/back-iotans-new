c7184d635182882785ccbe83699be77e8a051ec1
guadaso <saveliyrazdymakho@gmail.com>
2026-01-04 03:55:30 +0300

    init2

--- index.cjs
+++ index.cjs
@@ -16,13 +16,7 @@ const RESTRICTED_ROOT = 'public/templates/';
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-    'https://registry-converter.web.app'
-  ]
-}));
+app.use(cors({ origin: '*' }));
 
 // Статика — иконки
 app.use('/icons', express.static('public/icons'));
